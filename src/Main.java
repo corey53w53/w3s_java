63c573ff7255d7437968fd9800e0a3c72ecda219
Corey Wang <corefire2004@gmail.com>
2022-05-14 20:29:42 -0400

    method overloading

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        test();
+        System.out.println(add("hello","hi"));
+        System.out.println(add(3,5));
+    }
+    static String add(String a, String b){
+        for (int i = 0; i<a.length();i++){
+            System.out.println(a.charAt(i));
+        }
+        return a+b;
+    }
+    static int add(int a, int b){
+        return a+b;
     }
     static void test(){
         System.out.println("hi");
