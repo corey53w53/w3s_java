b8b50f859fa6da23b680adbe3a16bdf5a456062d
Corey Wang <corefire2004@gmail.com>
2022-05-14 20:25:19 -0400

    method

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x=30;
-        String a =  (x>25) ? "x is greater than 25" : "x is less than or equal to 25";
-        System.out.println(a);
-
-        int b = 5;
-        do{
-            System.out.println(b);
-            b++;
-        }while(b<7);
-
-        int[] ints = {1,2,3};
-        for (int c:ints){
-            /*
-            For each loops
-             */
-            System.out.println(c);
-        }
-
+        test();
+    }
+    static void test(){
+        System.out.println("hi");
     }
 }
